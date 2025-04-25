201bea525d20f4a391049d737f2a3800233a1927
Noxalas-Desktop <NatanielWlo@gmail.com>
2025-04-25 13:28:37 +0200

    variable name clarity

--- pet_feeder.py
+++ pet_feeder.py
@@ -25,7 +25,7 @@ def _on_message(client, userdata, msg):
         if payload == "feed":
             if not motor.is_busy:
                 motor.dispense_food()
-                publisher.publish_if_needed(force=True)
+                food_level_publisher.publish_if_needed(force=True)
     
     elif topic == "command/petfeeder/dispenser/portion":
         try:
@@ -36,30 +36,30 @@ def _on_message(client, userdata, msg):
             print(f"Invalid portion value: {payload}")
     
     elif topic == "command/petfeeder/status":
-        publisher.publish_if_needed(force=True)
+        food_level_publisher.publish_if_needed(force=True)
     
     else:
         print(f"Unknown command topic, {topic}, with payload, {payload}")
 
 
-client = mqtt.Client()
+mqtt_client = mqtt.Client()
 
-discovery_publisher = DiscoveryPublisher(client)
+discovery_publisher = DiscoveryPublisher(mqtt_client)
 
-motor = FoodDispenserMotor(client)
-sensor = FoodLevelSensor(client)
+motor = FoodDispenserMotor(mqtt_client)
+sensor = FoodLevelSensor(mqtt_client)
 
-publisher = FoodLevelPublisher(sensor, 300, 0.5)
-publisher.start()
+food_level_publisher = FoodLevelPublisher(sensor, 300, 0.5)
+food_level_publisher.start()
 
 if MQTT_USER and MQTT_PASS:
-    client.username_pw_set(MQTT_USER, MQTT_PASS)
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
 
-client.on_connect = _on_connect
-client.on_message = _on_message
+mqtt_client.on_connect = _on_connect
+mqtt_client.on_message = _on_message
 
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
 discovery_publisher.publish_configs()
 
-client.loop_forever()
+mqtt_client.loop_forever()
